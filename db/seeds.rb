cdcfac3fa870f1850df52f03c63c7cdff78ee016
Kher Yee <kheryee@live.com>
2015-08-14 18:12:19 +0800

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times {Voter.create}
+10.times {Referendum.create}
+100.times {Question.create(referendum_id: rand(9))}
+1000.times {Vote.create(upvote: rand(1), question_id: rand(99), voter_id: rand(9))}
